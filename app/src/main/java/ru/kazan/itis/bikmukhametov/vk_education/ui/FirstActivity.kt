1c55984ab9a38f0315b35e4b6efb9cff84cd0194
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2026-03-05 00:23:20 +0300

    init: FirstActivity.kt, SecondActivity add

--- app/src/main/java/ru/kazan/itis/bikmukhametov/vk_education/ui/FirstActivity.kt
+++ app/src/main/java/ru/kazan/itis/bikmukhametov/vk_education/ui/FirstActivity.kt
@@ -1,47 +1,31 @@
-package ru.kazan.itis.bikmukhametov.vk_education
+package ru.kazan.itis.bikmukhametov.vk_education.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.kazan.itis.bikmukhametov.vk_education.ui.theme.VK_EducationTheme
 
-class MainActivity : ComponentActivity() {
+class FirstActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VK_EducationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VK_EducationTheme {
-        Greeting("Android")
-    }
 }
